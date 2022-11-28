5a3c506cde04c17c77b7410c8a7316c0b9b57aaa
Etienne Bellé <bl.etienne7@gmail.com>
2022-11-28 11:10:18 +0100

    search event by host town

--- src/pages/JamSessions.jsx
+++ src/pages/JamSessions.jsx
@@ -1,19 +1,55 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 function JamSessions({ events }) {
 
+    const [jamsArr, setJamsArr] = useState([]);
+    const [filteredJams, setFilteredJams] = useState([]);
+
+    const sortEventsByDate = () => {
+        const jamsArr = [...events];
+        jamsArr.sort(function (a, b) {
+            return new Date(a.date) - new Date(b.date);
+        })
+        setJamsArr(jamsArr);
+    }
+
+    const handleSearchInput = (event) => {
+        setFilteredJams(event.target.value)
+    }
+
+    useEffect(() => {
+        sortEventsByDate();
+    }, [events])
+
     return (
         <div>
+
+            <div className='searchbar'>
+
+                <p>Search</p>
+                <input
+                    value={filteredJams}
+                    placeholder='Look for a jam in your town!'
+                    type='text'
+                    onChange={handleSearchInput}
+                />
+
+            </div>
+
             <h2>Upcoming Jam Sessions:</h2>
 
-            {events.map((event) => (
-                <div key={event._id}>
-                    <h3>{event.jamSessionName}</h3>
-                    <p>Created by: {event.host.barName}</p>
-                    <span>{event.date}</span>
-                    <span>{event.time}</span>
-                    <img src={event.image} />
+            {jamsArr
+                .filter(jam => {
+                    const filter = filteredJams.toLowerCase();
+                    return jam.host.town.toLowerCase().includes(filter)
+                })
+                .map((jam) => (
+                <div key={jam._id}>
+                    <h3>{jam.jamSessionName}</h3>
+                    <p>Created by: {jam.host.barName}</p>
+                    <p>Location: {jam.host.town}</p>
+                    <span>{jam.date}</span>
+                    <span>{jam.time}</span>
                 </div>
             ))}
             
