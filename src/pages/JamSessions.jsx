fd915ba842ae86f8d4e899acfb656879253c5288
Etienne Bellé <bl.etienne7@gmail.com>
2022-11-30 15:23:45 +0100

    start styling

--- src/pages/JamSessions.jsx
+++ src/pages/JamSessions.jsx
@@ -44,52 +44,69 @@ function JamSessions({ events }) {
 
     return jamsArr.length > 0 ? (
         <div>
+            <div className='searchbar-ctn'>
+                <div className='searchbar'>
 
-            <div className='searchbar'>
+                    <p>Search</p>
+                    <Input
+                        placeholder="What are you looking for?"
+                        type='text'
+                        value={filteredJams}
+                        onChange={handleSearchInput}
+                    />
+                    
+                    <DatePicker
+                        placeholder="Event date"
+                        label="Pick date"
+                        value={value}
+                        onChange={setValue}
+                    />
 
-                <p>Search</p>
-                <Input
-                    placeholder="What are you looking for?"
-                    type='text'
-                    value={filteredJams}
-                    onChange={handleSearchInput}
-                />
-                
-                <DatePicker
-                    placeholder="Event date"
-                    label="Pick date"
-                    value={value}
-                    onChange={setValue}
-                />
-
-            </div>
-
-            <h2>Upcoming Jam Sessions:</h2>
-
-            {jamsArr
-                .filter(jam => {
-                    if (!searchDate) {
-                        return true;
-                    }
-                    return jam.date === searchDate;
-                })
-                .filter(jam => {
-                    const filter = filteredJams.toLowerCase();
-                    return jam.host.town.toLowerCase().includes(filter)
-                        || jam.host.barName.toLowerCase().includes(filter)
-                        || jam.jamSessionName.toLowerCase().includes(filter)
-                })
-                .map((jam) => (
-                <Link to={`/events/${jam._id}`} key={jam._id}>
-                <div className='jam-item-ctn'>
-                    <h3>{jam.jamSessionName}</h3>
-                    <p>Created by: <Link to={`/locations/${jam.host._id}`}>{jam.host.barName}</Link></p>
-                    <p>Location: {jam.host.town}</p>
-                    <p>Date: {jam.date}</p>
-                    <p>Time: {jam.time.slice(16,21)}</p>
                 </div>
-                </Link>
-            ))}
+            </div>
+            
+            <section className='upcoming-events'>
+                <div className='section-title'>
+                    <h2>Upcoming Jam Sessions</h2>
+                </div>
+
+                {jamsArr
+                    .filter(jam => {
+                        if (!searchDate) {
+                            return true;
+                        }
+                        return jam.date === searchDate;
+                    })
+                    .filter(jam => {
+                        const filter = filteredJams.toLowerCase();
+                        return jam.host.town.toLowerCase().includes(filter)
+                            || jam.host.barName.toLowerCase().includes(filter)
+                            || jam.jamSessionName.toLowerCase().includes(filter)
+                    })
+                    .map((jam) => (
+                        
+                        <div className={`jam-session-card ${jam.genre.toLowerCase()}`} key={jam._id}>
+                            <Link to={`/events/${jam._id}`} >
+                                <div className='jam-img-ctn' >
+                                    <img src={jam.image} className="jam-img" alt='jam-session-image' />
+                                </div>
+                            </Link>
+                            <div className='jam-infos-ctn'>
+                                <div className='jam-name-ctn'>
+                                    <h3 className='jam-name'>{jam.jamSessionName}</h3>
+                                </div>
+                                <div className='jam-date-ctn'>
+                                    <p>{jam.date}</p>
+                                    <p>{jam.time.slice(16,21)}</p>
+                                </div>
+                                <div className='jam-host-ctn'>
+                                    <p>Created by: <Link to={`/locations/${jam.host._id}`}>{jam.host.barName}</Link></p>
+                                    <p>Location: {jam.host.town}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+            </section>
             
         </div>
     ) : (<></>)
