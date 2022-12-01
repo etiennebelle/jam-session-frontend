45290dd9ef8985db0613b80a12164f960b37ebaf
Etienne Bellé <bl.etienne7@gmail.com>
2022-12-01 19:46:44 +0100

    style home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,45 @@
 import { Link } from "react-router-dom";
+import { Button } from '@mantine/core';
 
 function Home() {
   return (
     <div>
-      <header>
+      <header className="home-header">
+        <div className="hero">
           <h1>"If music be the food of love, play on."</h1>
-          <p>Welcome to JAM! Here you can discover all the jam sessins happening around you.</p>
-          <Link to='/events'>Explore Jam Sessions</Link>
+        </div>
+        <div className="intro">
+          <p>Welcome to JAM! Here you can discover all the jam sessions happening around you.</p>
+        </div>
+        <div className="explore">
+          <Link to='/events'>
+            <Button color="dark" radius="xl">
+              Explore Jam Sessions
+            </Button>
+          </Link>
+        </div>
       </header>
-      <section>
-        <h2>How it works</h2>
-        <div>
+      <section className="home-main">
+        <div className="section-title">
+          <h2>How it works</h2>
+        </div>
+        <div className="home-card">
           <h3>For artists</h3>
           <p>Explore different jam sessions happening around you and sign up to play in them together with other artists!</p>
         </div>
-        <div>
+        
+        <div className="home-card">
           <h3>For hosts</h3>
           <p>Publish upcoming jam sessions event to have more visibility and to have artists playing in them.</p>
         </div>
-      </section>
-      <section>
-        <h2>Music can change the world because it can change people.</h2>
-        <Link to='/events'>Explore Jam Sessions</Link>
+        <div className="section-title">
+          <h2>Music can change the world because it can change people.</h2>
+        </div>
+        <Link to='/events'>
+            <Button color="dark" radius="xl">
+              Explore Jam Sessions
+            </Button>
+          </Link>
       </section>
     </div>
   )
